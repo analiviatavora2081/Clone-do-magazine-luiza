68007bb42565d2b5d75a41fda1940b86faca3cf5
analiviatavora2081 <analiviatavora2081@gmail.com>
2025-09-16 11:54:14 -0300

    Código de teste
    Fiz um código de teste

---
+++ Vetor.py
@@ -0,0 +1,21 @@
+vetor =["maça", "banana", "laranja"]
+
+#adiciona elementos na lista
+vetor.append ("uva")
+
+#limpa a lista
+vetor.clear ()
+
+#conta quantas vezeso elemento"maça" aparece na lista
+quantidade = vetor.count ("maça")
+
+#remove o primeiro elemento da lista
+vetor.remove(" banana")
+
+vetor.sort() #ordena a lista em ordem alfabética 
+
+#remove o elemento na posição 1 e retorma o elemento removido
+intem_removido = vetor.pop(1)
+
+vetor.clear() #remove todos os elementos da lista 
+print(item_removido)
